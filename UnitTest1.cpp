42538f2fee21fcc048c13b5369040e51e67d62d7
danaerapp <32438934+danaerapp@users.noreply.github.com>
2017-10-23 10:08:15 +0200

    Add files via upload

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -13,6 +13,7 @@ TEST (NeuronTest, ShouldNotSpike){
 	}
 	
 	EXPECT_EQ(neurone.getPotential(), potential);
+	EXPECT_EQ(0,neurone.getSpikesNumber()); //Should not have spiked
 	///EXPECT_EQ(neurone.getTime(0),); l'instruction neurone.getTime(0) should fail !
 }
 
@@ -32,5 +33,45 @@ for (int i(1); i < 1000; ++i){ //100 ms, On initialise i à 1, car le prermier u
 	}
 	
 	EXPECT_EQ(temps_spike,neurone.getTime(0)); 
+	EXPECT_EQ(1,neurone.getSpikesNumber());
+	
 }
 
+TEST (TwoNeurons, CorrespondanceWithJ){
+	Neuron neurone1;
+	Neuron neurone2;
+	double Iext(1.01);
+	
+	for (int i(1); i <= 924+Neuron::delay/0.1; ++i){
+		neurone1.update(Iext);
+		neurone2.update(0.0);
+		
+		if ( (neurone1.getPotential() >= Neuron::Vth) and not(neurone1.isRefractory())){ //Prendre le premier temps où le potentiel dépasse le threshold potential
+			neurone2.receive(0.1, Neuron::delay);
+		}
+	}
+	EXPECT_EQ(0.1,neurone2.getPotential());
+	EXPECT_EQ(0,neurone2.getSpikesNumber());
+	EXPECT_EQ(1,neurone1.getSpikesNumber());
+}
+
+TEST (TwoNeurons, TimeDelay){
+	Neuron neurone1;
+	Neuron neurone2;
+	double Iext(1.01);
+	int reception_time(0);
+	
+	for (int i(1); i <= 924+Neuron::delay/0.1; ++i){
+		neurone1.update(Iext);
+		neurone2.update(0.0);
+		
+		if ( (neurone1.getPotential() >= Neuron::Vth) and not(neurone1.isRefractory())){ //Prendre le premier temps où le potentiel dépasse le threshold potential
+			neurone2.receive(0.1, Neuron::delay);
+		}
+		
+		if (neurone2.getPotential() == 0.1){
+			reception_time = i;
+		}
+	}
+	EXPECT_EQ(924+Neuron::delay/0.1,reception_time);
+}
